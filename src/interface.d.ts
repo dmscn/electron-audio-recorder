4646f8ef74de236fc2c368f777a914c29d2175c2
Leo Damasceno <leo@damasceno.digital>
2025-04-20 12:27:30 -0300

    recreated eçectrpm app

--- src/interface.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IAudioAPI {
-    save: (wavArrayBuffer: ArrayBuffer, owner: string) => Promise<{ success: boolean; record?: { filename: string, data: string, owner: string }, error?: string }>,
-}
-
-declare global {
-    interface Window {
-        audioAPI: IAudioAPI
-    }
-}
--- src/preload.ts
+++
@@ -1,9 +0,0 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import { contextBridge, ipcRenderer } from 'electron';
-
-const audioAPI = {
-    save: (buffer: ArrayBuffer, owner: string) => ipcRenderer.invoke('save-audio', buffer, owner),
-}
-
-contextBridge.exposeInMainWorld('audioAPI', audioAPI);
